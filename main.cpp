a4b023287a9ae5ab5b6e8b5587855c2cd08a45f4
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2018-12-18 23:47:43 -0600

    Changelog :
    [1] Modified LED values to target NUCLEO_F767ZI User LEDs.
    
    [2] Implemented the TODO for nasapi_error_t translation to the appropriate string.
    
    [3] Plugged an over-looked memory leak.
    
    [4] Minor code formatting changes to improve readability.
    
    [5] Fix EasyConnect serial UART line endings by way of the platform object.

--- main.cpp
+++ main.cpp
@@ -43,25 +43,63 @@
 
 #define TIME_JWT_EXP      (60*60*24)  // 24 hours (MAX)
 
-// LED on/off - This could be different among boards
-#define LED_ON  0    
-#define LED_OFF 1
+// LED on/off - This could be different among boards. 
+// The following values do target the NUCLEO_F767ZI board.
+#define LED_ON  1    
+#define LED_OFF 0
 
 #include <string>
+#include <map>
 
-/* Flag to be set when a message needs to be published, i.e. BUTTON is pushed. */
+using ErrorCodesMap_t = std::map<nsapi_size_or_error_t, std::string>;
+using IndexElement_t  = ErrorCodesMap_t::value_type;
+
+static ErrorCodesMap_t make_error_codes_map()
+{
+    ErrorCodesMap_t eMap;
+    
+    eMap.insert(IndexElement_t(NSAPI_ERROR_OK, std::string("\"no error\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_WOULD_BLOCK, std::string("\"no data is not available but call is non-blocking\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_UNSUPPORTED, std::string("\"unsupported functionality\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_PARAMETER, std::string("\"invalid configuration\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_NO_CONNECTION, std::string("\"not connected to a network\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_NO_SOCKET, std::string("\"socket not available for use\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_NO_ADDRESS, std::string("\"IP address is not known\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_NO_MEMORY, std::string("\"memory resource not available\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_NO_SSID, std::string("\"ssid not found\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_DNS_FAILURE, std::string("\"DNS failed to complete successfully\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_DHCP_FAILURE, std::string("\"DHCP failed to complete successfully\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_AUTH_FAILURE, std::string("\"connection to access point failed\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_DEVICE_ERROR, std::string("\"failure interfacing with the network processor\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_IN_PROGRESS, std::string("\"operation (eg connect) in progress\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_ALREADY, std::string("\"operation (eg connect) already in progress\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_IS_CONNECTED, std::string("\"socket is already connected\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_CONNECTION_LOST, std::string("\"connection lost\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_CONNECTION_TIMEOUT, std::string("\"connection timed out\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_ADDRESS_IN_USE, std::string("\"Address already in use\"")));
+    eMap.insert(IndexElement_t(NSAPI_ERROR_TIMEOUT, std::string("\"operation timed out\"")));    
+    return eMap;
+}
+
+static ErrorCodesMap_t gs_ErrorCodesMap = make_error_codes_map();
+
+std::string ToString(const nsapi_size_or_error_t & key)
+{
+    return (gs_ErrorCodesMap.at(key));
+}
+
+// Flag to be set when a message needs to be published, i.e. BUTTON is pushed. 
 static volatile bool isPublish = false;
-/* Flag to be set when received a message from the server. */
+// Flag to be set when received a message from the server. 
 static volatile bool isMessageArrived = false;
-/* Buffer size for a receiving message. */
+// Buffer size for a receiving message. 
 const int MESSAGE_BUFFER_SIZE = 1024;
-/* Buffer for a receiving message. */
+// Buffer for a receiving message.
 char messageBuffer[MESSAGE_BUFFER_SIZE];
 
 // Function prototypes
 void handleMqttMessage(MQTT::MessageData& md);
 void handleButtonRise();
-
 
 int main(int argc, char* argv[])
 {
@@ -70,10 +108,12 @@ int main(int argc, char* argv[])
     const float version = 0.1;
 
     NetworkInterface* network = NULL;
-
-    DigitalOut led_red(LED_RED, LED_OFF);
-    DigitalOut led_green(LED_GREEN, LED_OFF);
-    DigitalOut led_blue(LED_BLUE, LED_OFF);
+    
+    // The following values do target the NUCLEO_F767ZI board.
+    // Reference : en.DM00244518.pdf
+    DigitalOut led_green(LED1);
+    DigitalOut led_blue(LED2);
+    DigitalOut led_red(LED3);
 
     printf("Mbed to Watson IoT : version is %.2f\r\n", version);
     printf("\r\n");
@@ -84,7 +124,8 @@ int main(int argc, char* argv[])
     printf("Opening network interface...\r\n");
     {
         network = easy_connect(true);    // If true, prints out connection details.
-        if (!network) {
+        if (!network) 
+	{
             printf("Unable to open network interface.\r\n");
             return -1;
         }
@@ -101,26 +142,31 @@ int main(int argc, char* argv[])
 
     std::string hostname = std::string(ORG_ID) + ".messaging.internetofthings.ibmcloud.com";
 
-    /* Establish a network connection. */
+    // Establish a network connection. 
     MQTTNetwork* mqttNetwork = NULL;
     printf("Connecting to host %s:%d ...\r\n", hostname.c_str(), MQTT_SERVER_PORT);
     {
         mqttNetwork = new MQTTNetwork(network);
         int rc = mqttNetwork->connect(hostname.c_str(), MQTT_SERVER_PORT, SSL_CA_PEM,
                 SSL_CLIENT_CERT_PEM, SSL_CLIENT_PRIVATE_KEY_PEM);
-        if (rc != MQTT::SUCCESS){
+        if (rc != MQTT::SUCCESS)
+	{
             const int MAX_TLS_ERROR_CODE = -0x1000;
-            // Network error
-            if((MAX_TLS_ERROR_CODE < rc) && (rc < 0)) {
-                // TODO: implement converting an error code into message.
-                printf("ERROR from MQTTNetwork connect is %d.", rc);
+            
+	    // Network error
+            if ((MAX_TLS_ERROR_CODE < rc) && (rc < 0)) 
+	    {
+                printf("ERROR from MQTTNetwork connect is: [%d] -> %s\n", rc, ToString(rc).c_str());
             }
-            // TLS error - mbedTLS error codes starts from -0x1000 to -0x8000.
-            if(rc <= MAX_TLS_ERROR_CODE) {
+            
+	    // TLS error - mbedTLS error codes starts from -0x1000 to -0x8000.
+            if (rc <= MAX_TLS_ERROR_CODE) 
+	    {
                 const int buf_size = 256;
                 char *buf = new char[buf_size];
                 mbedtls_strerror(rc, buf, buf_size);
                 printf("TLS ERROR (%d) : %s\r\n", rc, buf);
+		delete[] buf;
             }
             return -1;
         }
@@ -131,8 +177,7 @@ int main(int argc, char* argv[])
     const char* username = "use-token-auth";
     std::string cid = std::string("d:") + ORG_ID + ":" + DEVICE_TYPE + ":" + DEVICE_ID;
 
-
-    /* Establish a MQTT connection. */
+    // Establish a MQTT connection. 
     MQTT::Client<MQTTNetwork, Countdown, MQTT_MAX_PACKET_SIZE, MQTT_MAX_CONNECTIONS>* mqttClient = NULL;
     printf("MQTT client is connecting to the service ...\r\n");
     {
@@ -144,7 +189,8 @@ int main(int argc, char* argv[])
 
         mqttClient = new MQTT::Client<MQTTNetwork, Countdown, MQTT_MAX_PACKET_SIZE, MQTT_MAX_CONNECTIONS>(*mqttNetwork);
         int rc = mqttClient->connect(data);
-        if (rc != MQTT::SUCCESS) {
+        if (rc != MQTT::SUCCESS) 
+	{
             printf("ERROR: rc from MQTT connect is %d\r\n", rc);
             return -1;
         }
@@ -158,12 +204,13 @@ int main(int argc, char* argv[])
     const char* mqtt_topic_pub = "iot-2/evt/start/fmt/text";
     const char* mqtt_topic_sub = "iot-2/cmd/myevt/fmt/text";
 
-    /* Subscribe a topic. */
+    // Subscribe a topic.
     bool isSubscribed = false;
     printf("Client is trying to subscribe a topic \"%s\".\r\n", mqtt_topic_sub);
     {
         int rc = mqttClient->subscribe(mqtt_topic_sub, MQTT::QOS0, handleMqttMessage);
-        if (rc != MQTT::SUCCESS) {
+        if (rc != MQTT::SUCCESS) 
+	{
             printf("ERROR: rc from MQTT subscribe is %d\r\n", rc);
             return -1;
         }
@@ -178,23 +225,31 @@ int main(int argc, char* argv[])
     
     printf("To send a packet, push the button 1 on your board.\r\n");
 
-    /* Main loop */
-    while(1) {
-        /* Client is disconnected. */
-        if(!mqttClient->isConnected()){        
+    // Main loop
+    while (1) 
+    {
+        // Client is disconnected. 
+        if (!mqttClient->isConnected())
+	{        
             break;
         }
-        /* Waits a message and handles keepalive. */
-        if(mqttClient->yield(100) != MQTT::SUCCESS) {
+        
+	// Waits a message and handles keepalive. 
+        if (mqttClient->yield(100) != MQTT::SUCCESS) 
+	{
             break;
         }
-        /* Received a message. */
-        if(isMessageArrived) {
+	
+        // Received a message. 
+        if (isMessageArrived) 
+	{
             isMessageArrived = false;
             printf("\r\nMessage arrived:\r\n%s\r\n", messageBuffer);
         }
-        /* Button is pushed - publish a message. */
-        if(isPublish) {
+	
+        // Button is pushed - publish a message. 
+        if (isPublish) 
+	{
             isPublish = false;
             static unsigned int id = 0;
             static unsigned int count = 0;
@@ -214,10 +269,12 @@ int main(int argc, char* argv[])
             message.qos = MQTT::QOS0;
             message.id = id++;
             message.payloadlen = strlen(buf);
-            // Publish a message.
+            
+	    // Publish a message.
             printf("\r\nPublishing message to the topic %s:\r\n%s\r\n", mqtt_topic_pub, buf);
             int rc = mqttClient->publish(mqtt_topic_pub, message);
-            if(rc != MQTT::SUCCESS) {
+            if (rc != MQTT::SUCCESS) 
+	    {
                 printf("ERROR: rc from MQTT publish is %d\r\n", rc);
             }
             printf("Message published.\r\n");
@@ -230,20 +287,29 @@ int main(int argc, char* argv[])
 
     printf("The client has disconnected.\r\n");
 
-    if(mqttClient) {
-        if(isSubscribed) {
+    if (mqttClient) 
+    {
+        if (isSubscribed) 
+	{
             mqttClient->unsubscribe(mqtt_topic_sub);
             mqttClient->setMessageHandler(mqtt_topic_sub, 0);
         }
-        if(mqttClient->isConnected()) 
+	
+        if (mqttClient->isConnected())
+	{ 
             mqttClient->disconnect();
+	}
         delete mqttClient;
     }
-    if(mqttNetwork) {
+    
+    if (mqttNetwork) 
+    {
         mqttNetwork->disconnect();
         delete mqttNetwork;
     }
-    if(network) {
+    
+    if (network) 
+    {
         network->disconnect();
         // network is not created by new.
     }
@@ -268,6 +334,7 @@ void handleMqttMessage(MQTT::MessageData& md)
 /*
  * Callback function called when button is pushed.
  */
-void handleButtonRise() {
+void handleButtonRise() 
+{
     isPublish = true;
 }
